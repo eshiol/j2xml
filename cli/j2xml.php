3198cd3a93c648b607d18ab7d4a495345fec1b2b
Helios Ciancio <helios.ciancio@eshiol.it>
2016-04-30 17:39:37 +0200

    cli_j2xml-3.3.12

--- cli/j2xml.php
+++ cli/j2xml.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * @version		3.2.9 cli/j2xml.php
+ * @version		3.3.11 cli/j2xml.php
  * @package		J2XML.CLI
  * @subpackage	cli
  * @since		2.5
  *
  * @author		Helios Ciancio <info@eshiol.it>
  * @link		http://www.eshiol.it
- * @copyright	Copyright (C) 2010-2014 Helios Ciancio. All Rights Reserved
+ * @copyright	Copyright (C) 2010-2016 Helios Ciancio. All Rights Reserved
  * @license		http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL v3
  * J2XML is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
@@ -182,7 +182,8 @@ class J2XMLCli extends JApplicationCli
 			{
 				set_time_limit(120);
 				$params = JComponentHelper::getParams('com_j2xml');
-				J2XMLImporter::import($xml,$params);
+				$j2xml = new J2XMLImporter();
+				$j2xml->import($xml,$params);
 			}
 			else
 				$this->out(JText::sprintf('LIB_J2XML_MSG_FILE_FORMAT_NOT_SUPPORTED', $xmlVersion),'error');
